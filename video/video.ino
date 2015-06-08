572006552052e3ac6072f9f80f1fbc5ef9436365
Greg Winn <winn.greg@gmail.com>
2015-06-07 22:21:54 -0500

    Serial Keyboard folder created for serial port.

--- video/video.ino
+++ video/video.ino
@@ -10,7 +10,7 @@ int cSize = 50;
 int view_plane = 64;
 float angle = PI/60;
 void setup() {
-  TV.begin(NTSC,128,104);
+  TV.begin(NTSC,128,104); // SRAM used = 1,664
   TV.select_font(font4x6);
   TV.println("This is a 4x6 font test");
 }
